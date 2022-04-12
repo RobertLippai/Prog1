1533b3eb4b3855572b4beb819b936ac5d188618e
Lippai Róbert <robertcody.nl@gmail.com>
2022-04-12 23:05:08 +0200

    Updated Drill24

--- source/week10/drill/drill24.cpp
+++ source/week10/drill/drill24.cpp
@@ -3,10 +3,6 @@
 */
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <list> 
-#include <array> 
 #include "include/Matrix.h"
 #include "include/MatrixIO.h"
 #include <math.h>
@@ -14,89 +10,56 @@
 #include <errno.h>
 
 using namespace Numeric_lib;
-using Numeric_lib::Index;
 
-int main(){
-	// Task 1
-	std::cout << "Task 1" << std::endl;
-
-	std::cout << "Printing the size of char: " << sizeof(char) << std::endl;
-	std::cout << "Printing the size of short: " << sizeof(short) << std::endl;
-	std::cout << "Printing the size of int: " << sizeof(int) << std::endl;
-	std::cout << "Printing the size of long: " << sizeof(long) << std::endl;
-	std::cout << "Printing the size of float: " << sizeof(float) << std::endl;
-	std::cout << "Printing the size of double: " << sizeof(double) << std::endl;
-	std::cout << "Printing the size of int*: " << sizeof(int*) << std::endl;
+void task4(){
+	// only works on linux
+	errno = 0;
 	
-	std::cout << std::endl << std::endl;
-
-	// Task 2
-	std::cout << "Task 2" << std::endl;
-
-	Matrix<int> a(10);
-    Matrix<int> b(100);
-    Matrix<double> c(10);
-    Matrix<int,2> d(10,10);
-    Matrix<int,3> e(10,10,10);
-
-
-	std::cout << "Printing the size of Matrix a (sizeof): " << sizeof(a) << std::endl;
-	std::cout << "Printing the size of Matrix b (sizeof): " << sizeof(b) << std::endl;
-	std::cout << "Printing the size of Matrix c (sizeof): " << sizeof(c) << std::endl;
-	std::cout << "Printing the size of Matrix d (sizeof): " << sizeof(d) << std::endl;
-	std::cout << "Printing the size of Matrix e (sizeof): " << sizeof(e) << std::endl;	
-
-	std::cout << std::endl << std::endl;
-
-	// Task 3
-
-	std::cout << "Printing the size of Matrix a: " << a.size() << std::endl;
-	std::cout << "Printing the size of Matrix b: " << b.size() << std::endl;
-	std::cout << "Printing the size of Matrix c: " << c.size() << std::endl;
-	std::cout << "Printing the size of Matrix d: " << d.size() << std::endl;
-	std::cout << "Printing the size of Matrix e: " << e.size() << std::endl;	
-
-	// Task 4
-	std::cout << "Task 4: " << std::endl;
-	std::cout << "Please enter ints." << std::endl;
+	std::cout << "Please enter numbers to calculate square root. " << std::endl;
+	std::cout << "Enter any character to exit." << std::endl;
 
 	for(double input; std::cin >> input;){
-
-		if(input < 0){
+		auto result = sqrt(input);
+		
+		// errno doesn't seem to work on macos
+		if(errno || input < 0){
 			std::cout << "No Square Root!" << std::endl;
-		} else {
-			std::cout << sqrt(input) << std::endl;
+			//std::cout << "Errno info: " << errno << std::endl;
+			errno = 0;
+			continue;
 		}
+
+		std::cout << result << std::endl;
 	}
-
-
+	
 	std::cin.clear();
 	std::cin.ignore();
+	
+}
 
-	// Task 5
-	std::cout << "Task 5: " << std::endl;
-	std::cout << "Please enter 10 non-whole numbers." << std::endl;
+void task5(){
+	std::cout << "Please enter 10 floating-point number: " << std::endl;
 
-	Matrix<double> task5_m(10);
+	Matrix<double> double_matrix(10);
 
 	for (int i = 0; i < 10; ++i)
 	{
-		std::cin >> task5_m[i];
+		std::cin >> double_matrix[i];
 	}
 
-	std::cout << task5_m << std::endl;
+	std::cout << double_matrix << std::endl;
 
 	std::cin.clear();
 	std::cin.ignore();
+}
 
-	// Task 6
-
-	std::cout << "Task 6: " << std::endl;
-	std::cout << "Please enter 2 numbers to represent an n*m matrix. (Use spcaes to separet them.)" << std::endl;
+void task6(){
+	std::cout << "Enter 2 numbers to generate an n*m matrix. (Use spaces between the numbers.) " << std::endl;
 
 	int n, m;
 
-	std::cin >> n >> m;
+	std::cin >> n >> m;	
+	std::cout << std::endl;
 
 	Matrix<int, 2> multiplication_table(n, m);
 
@@ -104,7 +67,6 @@ int main(){
 	{
 		for (int j = 0; j < multiplication_table.dim2(); ++j)
 		{
-
 			if(j == 0 && i == 0){
 				multiplication_table(i, j) = 0;
 				continue;
@@ -120,8 +82,7 @@ int main(){
 
 			multiplication_table(i, j) = (i) * (j);
 			
-
-			//multiplication_table(i,j) = i == 0 || j == 0 ? i + j : i * j;
+			//multiplication_table(i, j) = (i+1) * (j+1);
 		}
 	}
 
@@ -129,56 +90,132 @@ int main(){
 	std::cin.ignore();
 
 
-	for(int i = 0; i < multiplication_table.dim1(); ++i){
-		for(int j = 0; j < multiplication_table.dim2(); ++j)
+	for (int i = 0; i < multiplication_table.dim1(); ++i){
+	
+		for (int j = 0; j < multiplication_table.dim2(); ++j){
 			std::cout << multiplication_table(i,j) << '\t';
-		std::cout << '\n';
+		}
+		
+		std::cout << std::endl;
 	}
+	
+	std::cout << std::endl;
+}
 
-	// Task 7
-	std::cout << "Task 7: " << std::endl;
-	std::cout << "Please enter 10 complex numbers " << std::endl;
+void task7(){
+	std::cout << "Enter 10 complex numbers\nFormat (real,imag): " << std::endl;
 
 	Matrix<std::complex<double>> complex_matrix (10);
 
-	std::complex<double> temp_c;
-
 	for (int i = 0; i < 10; ++i)
 	{
-		std::cin >> temp_c;
-		complex_matrix[i] = temp_c;
+		std::cin >> complex_matrix[i];
 	}
-
+	
+	std::cout << std::endl;
+	
 	std::complex<double> sum;
 
-	for (Index i = 0; i < complex_matrix.dim1(); ++i)
+	for (int i = 0; i < complex_matrix.dim1(); ++i)
 	{
 		sum += complex_matrix[i];
 	}
-	std::cout << "Sum of the complex matrix: " << sum << std::endl;
+	
+	std::cout << "Complex matrix sum\nReal part: " << sum.real() << "\tImaginary part: " << sum.imag() << std::endl;
 
-
+	std::cout << std::endl;
+	
 	std::cin.clear();
 	std::cin.ignore();
+}
 
-	// Task 8
+void task8(){
+	std::cout << "Please enter 6 whole numbers: " << std::endl;
 
-	std::cout << "Task 8: " << std::endl;
-	std::cout << "Please enter 6 ints " << std::endl;
+	Matrix<int, 2> int_matrix (2,3);
 
-	Matrix<int, 2> task8_matrix (2,3);
-
-	for (int i = 0; i < task8_matrix.dim1(); ++i)
+	for (int i = 0; i < int_matrix.dim1(); ++i)
 	{
-		for (int j = 0; j < task8_matrix.dim2(); ++j)
+		for (int j = 0; j < int_matrix.dim2(); ++j)
 		{
-			int temp;
-			std::cin >> temp;
-			task8_matrix(i,j) = temp;
+			std::cin >> int_matrix(i,j);
 		}
 	}
 
-	std::cout << "Outputing matrix: " << task8_matrix << std::endl;
+	std::cout << "Printing matrix: " << int_matrix << std::endl;
+}
+
+
+int main(){
+	// Task 1
+	std::cout << "Task 1" << std::endl;
+
+	std::cout << "Printing the size of char: " << sizeof(char) << std::endl;
+	std::cout << "Printing the size of short: " << sizeof(short) << std::endl;
+	std::cout << "Printing the size of int: " << sizeof(int) << std::endl;
+	std::cout << "Printing the size of long: " << sizeof(long) << std::endl;
+	std::cout << "Printing the size of float: " << sizeof(float) << std::endl;
+	std::cout << "Printing the size of double: " << sizeof(double) << std::endl;
+	std::cout << "Printing the size of int*: " << sizeof(int*) << std::endl;
+	std::cout << "Printing the size of double*: " << sizeof(double*) << std::endl;
+	
+	std::cout << std::endl << std::endl;
+
+	// Task 2
+	std::cout << "Task 2" << std::endl;
+
+	Matrix<int> a(10);
+	Matrix<int> b(100);
+	Matrix<double> c(10);
+	Matrix<int,2> d(10,10);
+	Matrix<int,3> e(10,10,10);
+	
+	// sizeof in memory
+	
+	std::cout << "The size of Matrix a (sizeof): " << sizeof(a) << std::endl;
+	std::cout << "The size of Matrix b (sizeof): " << sizeof(b) << std::endl;
+	std::cout << "The size of Matrix c (sizeof): " << sizeof(c) << std::endl;
+	std::cout << "The size of Matrix d (sizeof): " << sizeof(d) << std::endl;
+	std::cout << "The size of Matrix e (sizeof): " << sizeof(e) << std::endl;	
+
+	std::cout << std::endl << std::endl;
+
+	// Task 3
+	std::cout << "Task 3" << std::endl;
+	
+	std::cout << "The number of elements in Matrix a: " << a.size() << std::endl;
+	std::cout << "The number of elements in Matrix b: " << b.size() << std::endl;
+	std::cout << "The number of elements in Matrix c: " << c.size() << std::endl;
+	std::cout << "The number of elements in Matrix d: " << d.size() << std::endl;
+	std::cout << "The number of elements in Matrix e: " << e.size() << std::endl;	
+
+
+	// Task 4
+	std::cout << "Task 4" << std::endl;
+	
+	task4();
+
+	// Task 5
+	std::cout << "Task 5" << std::endl;
+	
+	task5();
+
+	// Task 6
+
+	std::cout << "Task 6" << std::endl;
+	
+	task6();
+
+	// Task 7
+	std::cout << "Task 7" << std::endl;
+	
+	task7();
+
+	// Task 8
+
+	std::cout << "Task 8 " << std::endl;
+	
+	task8();
 
 	return 0;
 }
